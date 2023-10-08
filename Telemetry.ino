3c1483d6a6e06a4db76e70cb398fe24d0b282e85
Tom <tommytom2006@icloud.com>
2023-10-08 10:55:53 +0200

    Create Telemetry.ino

---
+++ Telemetry.ino
@@ -0,0 +1,190 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "GY521.h"
+#include <SD.h>
+
+
+//Screen Setup
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+//SD Setup
+File myFile;
+bool noSD = true;
+
+//RPM Vars
+const int rpmPin = 0;
+
+const float diameter = 0.001; //km
+const float circumference = 2 * 3.1415926 * diameter;
+
+volatile unsigned long prevTimeRPM = 0; //Volatile as it is used both in the interrupt and main loop
+volatile unsigned long lowTimeRPM = 0;
+volatile int RPM = 0;
+
+
+//Gyro Vars
+GY521 sensor(0x69); //If AD0 Connected to GND, 0x68, if connected to VCC 0x69
+const int btnPin = 2;
+volatile float angleGZ = 0;
+volatile float angleGY = 0;
+volatile float angleGX = 0;
+
+//Loop Vars
+int counter = 0;
+int prevtime = 0;
+int curtime = 0;
+
+
+void setup() {
+  delay(100);
+  Serial.begin(115200);
+  // ---- Interupt and Pin Setup ----
+  pinMode(btnPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(btnPin), resetGyro, RISING);
+  pinMode(rpmPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(rpmPin), RPM_Func, FALLING);
+
+  // ---- Screen Setup ----
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  Serial.println(F("Screen Setup"));
+  
+  delay(1000); // Pause for 1 second
+
+  // Clear the buffer
+  display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+
+  // ---- Screen Setup ----
+  while (sensor.wakeup() == false)
+  {
+    Serial.print(millis());
+    Serial.println(F("\tCould not connect to GY521"));
+    delay(1000);
+  }
+  sensor.setAccelSensitivity(0);  //  2g
+  sensor.setGyroSensitivity(0);   //  250 degrees/s
+  sensor.setThrottle();
+
+  // Set calibration
+  sensor.gxe = 1;
+  sensor.gye = 1;
+  sensor.gze = 0;
+  Serial.println(F("Gyro Setup"));
+
+  // ---- SD Setup ----
+  if (!SD.begin(15)) {
+    Serial.println("SD initialization failed!");
+    return;
+  }
+  Serial.println("SD Setup");
+
+  
+}
+
+
+void loop() {
+  display.clearDisplay();
+  curtime = millis();
+  sensor.read();
+  int gx = sensor.getGyroX();
+  int gy = sensor.getGyroY();
+  int gz = sensor.getGyroZ();
+
+  int deltaTime = curtime - prevtime;
+  float deltaGZ = deltaTime * gz;
+  angleGZ += deltaGZ / 1000;
+  float deltaGY = deltaTime * gy;
+  angleGY += deltaGY / 1000;
+  float deltaGX = deltaTime * gx;
+  angleGX += deltaGX / 1000;
+
+  //DateTime now = rtc.now();
+  display.setCursor(0,0);
+  display.println(millis());
+  display.print(F("X Rot:"));
+  display.println(angleGX);
+  display.print(F("Y Rot:"));
+  display.println(angleGY);
+  display.print(F("Z Rot:"));
+  display.println(angleGZ); 
+  display.print(F("RPM:"));
+  display.println(RPM); 
+  if(counter % 10 == 0){ 
+
+    myFile = SD.open("TestVals.csv", FILE_WRITE);
+    if (myFile) {
+        myFile.print(millis());
+        myFile.print(",");
+        myFile.print(angleGX);
+        myFile.print(",");
+        myFile.print(angleGY);
+        myFile.print(",");
+        myFile.println(angleGZ);
+        myFile.print(",");
+        myFile.println(RPM); 
+        //Serial.println("Update SD");
+        noSD = false;
+      } else {
+        //Serial.println("SD Failed");
+        noSD = true;
+      }
+    myFile.close();
+    
+  }
+  if(noSD == true){
+    display.println("NO SD CARD");
+  }
+  
+  if (digitalRead(rpmPin) == HIGH){ // Stops 'bouncing' of the hall effect sensor. The reason why there is a difference between 'prevTime' and 'lowTime' is because the former is the time the moment a magnet was detected, while the latter changes to the latest time a magnet was detected. Ask Tom Brouwers for more information if necessary.
+    lowTimeRPM = millis();
+  } else if (millis() - lowTimeRPM > 3000){
+    RPM = 0;
+  }
+
+  counter++;
+  prevtime = curtime;
+  display.display();
+  delay(30); 
+} //Total delays: 30 ms
+
+//HELPER FUNCTIONS
+
+/*
+
+void recSD(DateTime now){
+  
+
+}
+*/
+
+//INTERUPT FUNCTIONS
+
+ICACHE_RAM_ATTR void resetGyro(){
+  angleGZ = 0;
+  angleGY = 0;
+  angleGX = 0;
+  Serial.println("Reset");
+} 
+
+ICACHE_RAM_ATTR void RPM_Func(){ // Gets called when D4 is high (When the hall effect sensor senses a magnetic field)
+  Serial.println("RPM update");
+  unsigned long curTimeRPM = millis();
+  if (curTimeRPM - lowTimeRPM > 50){ // If time between previous 'HIGH' state of the sensor is greater than 100 ms (Debouncing)
+    RPM = 60000/(curTimeRPM - prevTimeRPM); // 1 Minute (in ms) over time of one rotation (ms)
+   
+  }
+  prevTimeRPM = curTimeRPM; // Initial trigger time
+  lowTimeRPM = curTimeRPM;
+}
+
