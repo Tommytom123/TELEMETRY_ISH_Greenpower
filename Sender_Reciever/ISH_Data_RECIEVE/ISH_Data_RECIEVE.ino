5302ab6a4b7e3ea5c634a8a21231ff98150784bc
Tommytom123 <tommytom2006@icloud.com>
2023-12-02 10:53:03 +0100

    MAJOR CHANGES - Improved collaborative Framework

--- Sender_Reciever/ISH_Data_RECIEVE/ISH_Data_RECIEVE.ino
+++ Sender_Reciever/ISH_Data_RECIEVE/ISH_Data_RECIEVE.ino
@@ -1,5 +1,6 @@
+// Code for the front Micro Controller. Written by Tom Brouwers
+// Please ONLY edit inside the function for your task. If you want to edit other things, please ask Tom.
 
-//#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <SD.h>
@@ -8,70 +9,70 @@
 
 #define SERIAL_BUFFER_SIZE 64
 
-//Screen Setup
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Screen Setup
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-//PINS
-const int THROTTLE_PIN_IN = 32;
+// PINS
 const int BTN_PIN_LEFT = 33;
 const int BTN_PIN_RIGHT = 25;
-// const int MODE_SWITCH = ; // Commented out as I need more pins :\ . 
+const int MODE_PIN = 32; // Uses the adc and V deviders to allow for everything to work off of one singular pin :) Explaination should be on document
+
+// const int MODE_SWITCH = ; // Commented out as I need more pins :\ .
 // Likely a 3 way switch where we can change the car driving mode - Max Efficiency, Slow, Max Speed
 
-//SD Setup
+// SD Setup
 File myFile;
 bool SD_in = false;
-//uint64_t SD_space = 0;
 
-//RPM Vars
-float throttlePerc = 0;
+// Main Vars
+int MODE = 0;
+float throttle_perc = 0;
+int RPM;
 
-//Loop Vars
+// Loop Vars
 int counter = 0;
 volatile int curTime = 0;
 volatile int debounceTime = 0;
 int page = 0;
-float X, Y, Z;
-int R;
+
 String data;
 
+//Second Serial Pins
 #define RXD2 16
 #define TXD2 17
 
 
+// ---- INTERRUPT FUNCTIONS ----
 
-// INTERUPTS FOR BUTTONS AND SUCH
-
-  void IRAM_ATTR BTN_PRESSED_LEFT(){
-    curTime = millis();
-    if (curTime - debounceTime > 100){
-      page += 1;
-      debounceTime = curTime;
-    }
-      
+void IRAM_ATTR BTN_PRESSED_LEFT() {
+  curTime = millis();
+  if (curTime - debounceTime > 100) {
+    page += 1;
+    debounceTime = curTime;
   }
+}
 
-  void IRAM_ATTR BTN_PRESSED_RIGHT(){
-    int curTime = millis();
-    if (curTime - debounceTime > 100){
-      page += -1;
-      debounceTime = curTime;
-    }
+void IRAM_ATTR BTN_PRESSED_RIGHT() {
+  int curTime = millis();
+  if (curTime - debounceTime > 100) {
+    page += -1;
+    debounceTime = curTime;
   }
+}
 
-// RUN ON INITIALIZATION
+// ---- RUN ON INITIALIZATION ----
 
 void setup() {
   delay(100);
-  Serial.begin(9600);
-  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.begin(9600); // Communication to other board
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2); // Communication to HC12
 
-  // ---- Interupt and Pin Setup ----
+  // - Interupt and Pin Setup -
 
   pinMode(BTN_PIN_LEFT, INPUT_PULLUP);
   pinMode(BTN_PIN_RIGHT, INPUT_PULLUP);
@@ -79,14 +80,15 @@ void setup() {
   attachInterrupt(BTN_PIN_LEFT, BTN_PRESSED_LEFT, RISING);
   attachInterrupt(BTN_PIN_RIGHT, BTN_PRESSED_RIGHT, RISING);
 
-  // ---- SD Setup ----
-  
+  // - SD Setup -
+
   INIT_SD();
 
-  // ---- Screen Setup ----
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  // - Screen Setup -
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ;  // Don't proceed, loop forever
   }
   Serial.println(F("Screen Setup"));
 
@@ -96,145 +98,163 @@ void setup() {
   display.setTextSize(1);
   display.println("SETUP");
   display.display();
+
   delay(1000);
- 
 }
 
-void loop() { //One loop takes about 30ms
-  display.clearDisplay();
-  
-  SERIAL_READ();  
+// ---- MAIN LOOP ----
 
-  if(counter % 3 == 0){  
+void loop() {
+  display.clearDisplay();
+
+  SERIAL_READ();
+  READ_MODE();
+  if (counter % 3 == 0) {
     WRITE_SD();
   }
 
   DISPLAY_FUNC();
-  
+
   counter++;
   display.display();
 }
 
 
-//HELPER FUNCTIONS
-void DISPLAY_FUNC(){
-  display.setCursor(0,0);
-  
-  if (page == 0){ //Page 1 --
-    
-    display.println(millis());
-    display.println(X);
-    display.println(Y);
-    display.println(Z);
-    display.println(R);
-    GET_THROT_PERC();
-    display.print(F("Throttle:"));
-    display.println(throttlePerc); 
+// ---- HELPER FUNCTIONS ----
 
-    if(digitalRead(BTN_PIN_LEFT) == LOW){
-      display.println("Left BTN HIGH");
-    }
-    if(digitalRead(BTN_PIN_RIGHT) == LOW){
-      display.println("Right BTN HIGH");
-    }
-  
-  } else if(page == 1){ //Page 2
-    display.println("Page 2");
+void DISPLAY_FUNC() {
+  display.setCursor(0, 0);
 
-  } else{
-    display.println("ERROR with the page");
-  } 
-}
+  switch (page){
+    case 0: // Home page
+      display.println(millis());
+      display.println(RPM);
+      display.println(MODE);
+      display.print(F("Throttle:"));
+      display.println(throttle_perc);
 
-void GET_THROT_PERC(){
-  int throttle = analogRead(THROTTLE_PIN_IN);
-  throttlePerc = (throttle - 50) / (4095.0 - 50); // Find perc range = ((input - min) * 100) / (max - min) | For the min value, we would want it to be slightly higher than the lowest measurable value to avoid any jitter.
-  if (throttlePerc < 0) {
-    throttlePerc = 0;
+      if (digitalRead(BTN_PIN_LEFT) == LOW) {
+        display.println("Left BTN HIGH");
+      }
+      if (digitalRead(BTN_PIN_RIGHT) == LOW) {
+        display.println("Right BTN HIGH");
+      }
+      break;
+
+    case 1: // Settings?
+      display.println("Page 2");
+      break;
+
+    case 2: // Graphs / data
+      display.println("Page 3");
+      break;
+
+    case 3: // Graphs / data
+      display.println("Page 4");
+      break;
+
+    default:
+      display.println("ERROR with the page");
+      break;
   }
 }
-// Sd card stuff
-void INIT_SD(){
-  if(!SD.begin(5)){
-      Serial.println("Card Mount Failed");
-      return;
+
+// -- Mode
+
+void READ_MODE() {
+  int m_analog = analogRead(MODE_PIN);
+  // NOT FINAL VERSION AT ALL. Calculate threshold values based on the resistor values and what V they should result in in the 12? bit adc
+  //display.println(m_analog);
+  if (m_analog < 2000){
+    MODE = 0;
+  } else if (m_analog < 4095){
+    MODE = 1;
+  }
+}
+
+// -- Sd card stuff
+void INIT_SD() {
+  if (!SD.begin(5)) {
+    Serial.println("Card Mount Failed");
+    return;
   }
   uint8_t cardType = SD.cardType();
 
   // filename = "/Telem.csv" // We need a function here that automatically gets a new filename every time the ESP restarts, so it doesn't overwrite anything
 
-  if(cardType == CARD_NONE){
+  if (cardType == CARD_NONE) {
     Serial.println("No SD card attached");
     SD_in = false;
   } else {
     SD_in = true;
     myFile = SD.open("/Telem.csv", FILE_WRITE);
-    if(!myFile) {
-      myFile.println("Time,Bat1V,Bat2V,Current"); // Initializes the data CSV with header
+    if (!myFile) {
+      myFile.println("Time,Bat1V,Bat2V,Current");  // Initializes the data CSV with header
     }
     myFile.close();
     Serial.println("SD Setup");
   }
-  
+
   // Currently no check to see how much space left, and to stop writing when a threshold is reached. Currently just ensure that the card is empty before running and hope for the best
 
   //SD_space = SD.cardSize() / (1024 * 1024);
   //Serial.printf("SD Card Size: %lluMB\n", cardSize);
 }
 
-void WRITE_SD(){
-  
-  if (SD_in == true) {   
-    myFile = SD.open("/Telem.csv",  FILE_APPEND);
+void WRITE_SD() {
+
+  if (SD_in == true) {
+    myFile = SD.open("/Telem.csv", FILE_APPEND);
     if (myFile) {
       display.println("WRITING");
-      
-      myFile.print(millis()); 
+
+      myFile.print(millis());
       myFile.print(",");
       myFile.print("Data,");
       myFile.println("WORKING");
-      
+
       myFile.close();
     } else {
       SD_in = false;
       display.println("SD CARD error");
-    } 
+    }
   }
 }
 
+// -- HC12
+
+void TRANSMIT_DATA() {
+  // Add the code to send data via the HC12 to the pits
+}
+
+// -- Serial
+
 // Read data from other microcontroller
 void SERIAL_READ() {
-  while (Serial2.available()>0) {
-    char incomingChar = Serial2.read();
+  while (Serial.available() > 0) {
+    char incomingChar = Serial.read();
     if (incomingChar == '\n') {
       // Newline character detected, break the loop and print the received data
       break;
     }
-  } 
- 
+  }
+
 
   // Check if there's serial data available
-  if (Serial2.available()) {
-    data = Serial2.readStringUntil('\n'); // Read data until a newline character
-    int z_pos = data.indexOf("Z");
-    int y_pos = data.indexOf("Y");
-    int x_pos = data.indexOf("X");
-    int r_pos = data.indexOf("R");
+  if (Serial.available()) {
+    data = Serial.readStringUntil('\n');  // Read data until a newline character
+    int T_pos = data.indexOf("T");
+    int R_pos = data.indexOf("R");
 
-    if (z_pos != -1 && y_pos != -1 && x_pos != -1 && r_pos != -1) {
-        Z = data.substring(z_pos + 1, y_pos).toFloat();
-        Y = data.substring(y_pos + 1, x_pos).toFloat();
-        X = data.substring(x_pos + 1, r_pos).toFloat();
-        R = data.substring(r_pos + 1).toFloat();
+    if (T_pos != -1 && R_pos != -1) {
+      throttle_perc = data.substring(T_pos + 1, R_pos).toFloat();
+      RPM = data.substring(R_pos + 1).toFloat();
     } else {
-        // Handle the case where the string format is not as expected
-        //Z = Y = X = R = 0.0; // You can set default values or handle errors as needed
+      // Handle the case where the string format is not as expected
     }
   } else {
-    //Z = Y = X = R = 0.0;
   }
-  while (Serial2.available()>30) { // Solves some buffer overflow issues
-    char incomingChar = Serial2.read();
+  while (Serial.available() > 30) {  // Solves some buffer overflow issues
+    char incomingChar = Serial.read();
   }
 }
-  
--- Sender_Reciever/ISH_Data_SENDER/ISH_Data_SENDER.ino
+++ Sender_Reciever/ISH_Data_SENDER/ISH_Data_SENDER.ino
@@ -1,87 +1,31 @@
-#include <GY521.h>
+// Code for the rear Micro Controller. Written by Tom Brouwers
+// Please ONLY edit inside the function for your task. If you want to edit other things, please ask Tom.
+
 #include <Wire.h>
 
+// PINS
+const int RPM_PIN = 5;
+const int THROTTLE_PIN_IN = 32; // ADC where Potentiometer is connected to
 
-const int rpmPin = 2;
+// Variables
+volatile int RPM = 0;
 volatile unsigned long lowTimeRPM = 0; //Volatile as it is used in interrupt
 volatile unsigned long prevTimeRPM = 0;
-volatile int RPM = 0;
 
-int prevtime = 0;
-int curtime = 0;
+float throttle_perc = 0;
 
-//Gyro Vars
-//GY521 sensor(0x69); //If AD0 Connected to GND, 0x68, if connected to VCC 0x69
-//const int btnPin = 2;
-volatile float angleGZ = 1;
-volatile float angleGY = 2;
-volatile float angleGX = 3;
- 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Wire.begin();
-
-  pinMode(rpmPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(rpmPin), RPM_Func, RISING);
-  
 /*
-  while (sensor.wakeup() == false)
-  {
-    Serial.print(millis());
-    Serial.println(F("\tCould not connect to GY521"));
-    delay(1000);
-  } */
-  //sensor.setAccelSensitivity(0);  //  2g
- // sensor.setGyroSensitivity(0);   //  250 degrees/s
- // sensor.setThrottle();
-/*
-  // Set calibration
-  sensor.axe = -0.0131885;
-  sensor.aye = 0.0028174;
-  sensor.aze = -1.0326611;
-  sensor.gxe = 1.8501527;
-  sensor.gye = 1.1008397;
-  sensor.gze = 0.2435878; */
-  Serial.println(F("Gyro Setup"));
-}
+motorTemp
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  curtime = millis(); 
-  //sensor.read();
-  int gx = 1; //sensor.getGyroX();
-  int gy = 2; //sensor.getGyroY();
-  int gz = 0;//sensor.getGyroZ();
+*/
 
-  int deltaTime = curtime - prevtime;
-  float deltaGZ = deltaTime * gz;
-  angleGZ += deltaGZ / 1000;
-  float deltaGY = deltaTime * gy;
-  angleGY += deltaGY / 1000;
-  float deltaGX = deltaTime * gx;
-  angleGX += deltaGX / 1000;
+// Serial Read Vars
+String data;
+int MODE = 0;
 
-  if (digitalRead(rpmPin) == HIGH){ // Stops 'bouncing' of the hall effect sensor. The reason why there is a difference between 'prevTime' and 'lowTime' is because the former is the time the moment a magnet was detected, while the latter changes to the latest time a magnet was detected. Ask Tom Brouwers for more information if necessary.
-    lowTimeRPM = millis();
-  } else if (millis() - lowTimeRPM > 3000){
-    RPM = 0;
-  }
-  //Serial.println("Start");
-  Serial.print("Z");
-  Serial.print(millis());
-  Serial.print("Y");
-  Serial.print(angleGY);
-  Serial.print("X");
-  Serial.print(angleGX);
-  Serial.print("R");
-  Serial.println(RPM);
+// ---- INTERRUPT FUNCTIONS ----
 
-  prevtime = curtime;
-  delay(10);
-}
-
-ICACHE_RAM_ATTR void RPM_Func(){ // Gets called when D4 is high (When the hall effect sensor senses a magnetic field)
+void IRAM_ATTR RPM_Func(){ // Gets called when D4 is high (When the hall effect sensor senses a magnetic field)
   
   unsigned long curTimeRPM = millis();
   if (curTimeRPM - lowTimeRPM > 40){ // If time between previous 'HIGH' state of the sensor is greater than 100 ms (Debouncing)
@@ -92,3 +36,98 @@ ICACHE_RAM_ATTR void RPM_Func(){ // Gets called when D4 is high (When the hall e
   }
   
 } 
+
+// ---- RUN ON INITIALIZATION ----
+
+void setup() {
+  Serial.begin(9600); // Communication to front MC
+
+  pinMode(RPM_PIN, INPUT);
+  attachInterrupt(RPM_PIN, RPM_Func, RISING);
+  
+  Serial.println(F("Gyro Setup"));
+}
+
+void loop() {
+  //int curtime = millis(); 
+
+
+  if (digitalRead(RPM_PIN) == HIGH){ // Stops 'bouncing' of the hall effect sensor. The reason why there is a difference between 'prevTime' and 'lowTime' is because the former is the time the moment a magnet was detected, while the latter changes to the latest time a magnet was detected. Ask Tom Brouwers for more information if necessary.
+    lowTimeRPM = millis();
+  } else if (millis() - lowTimeRPM > 3000){
+    RPM = 0;
+  }
+  
+  GET_THROT_PERC();
+
+  READ_SERIAL();
+  SEND_SERIAL();
+
+  
+  delay(10);
+}
+
+// ---- HELPER FUNCTIONS ----
+
+
+// -- Reading sensors
+void GET_THROT_PERC() {
+  int throttle = analogRead(THROTTLE_PIN_IN);
+  throttle_perc = (throttle - 50) / (4095.0 - 50);  // Find perc range = ((input - min) * 100) / (max - min) | For the min value, we would want it to be slightly higher than the lowest measurable value to avoid any jitter.
+  if (throttle_perc < 0) {
+    throttle_perc = 0;
+  }
+}
+
+void READ_ADC(){
+  // Reads the ADC values 
+  // Temperature 1
+  // Temp 2
+  // Bat V 1
+  // Bat V 2
+  // Current
+  //Throttle?
+
+}
+
+// -- Motor Controller
+
+void DRIVE_CONTROLLER(){
+}
+
+// -- SERIAL
+
+void SEND_SERIAL(){ // Sends data to front microcontroller
+  Serial.print("T");
+  Serial.print(throttle_perc);
+  Serial.print("R");
+  Serial.println(RPM);
+}
+
+// Read data from other microcontroller
+void READ_SERIAL() { // Reads the throttle Mode
+  while (Serial.available() > 0) {
+    char incomingChar = Serial.read();
+    if (incomingChar == '\n') {
+      // Newline character detected, break the loop and print the received data
+      break;
+    }
+  }
+
+  // Check if there's serial data available
+  if (Serial.available()) {
+    data = Serial.readStringUntil('\n');  // Read data until a newline character
+    int M_pos = data.indexOf("M");
+    if (M_pos != -1) {
+      MODE = data.substring(M_pos).toFloat();
+
+    } else {
+     // You can set default values or handle errors as needed
+    }
+  } else {
+    
+  }
+  while (Serial.available() > 30) {  // Solves some buffer overflow issues
+    char incomingChar = Serial.read();
+  }
+}
