8d64c8434764c6e564fd16bef652dc068a3f7224
Elemma21 <elesopha@gmail.com>
2024-01-31 16:15:19 +0100

    Changed Button

--- MicroControllers/Individual Files/VoltageGraph_u8g2_V2/VoltageGraph_u8g2_V2.ino
+++ MicroControllers/Individual Files/VoltageGraph_u8g2_V2/VoltageGraph_u8g2_V2.ino
@@ -1,51 +1,9 @@
-/*
 
-  HelloWorld.ino
-
-  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
-
-  Copyright (c) 2016, olikraus@gmail.com
-  All rights reserved.
-
-  Redistribution and use in source and binary forms, with or without modification, 
-  are permitted provided that the following conditions are met:
-
-  * Redistributions of source code must retain the above copyright notice, this list 
-    of conditions and the following disclaimer.
-    
-  * Redistributions in binary form must reproduce the above copyright notice, this 
-    list of conditions and the following disclaimer in the documentation and/or other 
-    materials provided with the distribution.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
-  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
-  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
-  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
-  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
-
-*/
 
 #include <Arduino.h>
 #include <U8g2lib.h>
 
-/*#include <TimerInterrupt.hpp>         //https://github.com/khoih-prog/TimerInterrupt
-#include <ISR_Timer.hpp>              //https://github.com/khoih-prog/TimerInterrupt
 
-// Select the timers you're using, here ITimer1
-#define USE_TIMER_1     true
-#define USE_TIMER_2     false
-#define USE_TIMER_3     false
-#define USE_TIMER_4     false
-#define USE_TIMER_5     false
-*/
 #define ARRAYSIZE 100
 #define PIXELRANGE 50
 #define VOLTRANGE 12
@@ -58,20 +16,114 @@
 #endif
 
 
-/*
-  U8g2lib Example Overview:
-    Frame Buffer Examples: clearBuffer/sendBuffer. Fast, but may not work with all Arduino boards because of RAM consumption
-    Page Buffer Examples: firstPage/nextPage. Less RAM usage, should work with all Arduino boards.
-    U8x8 Text Only Example: No RAM usage, direct communication with display controller. No graphics, 8x8 Text only.
-    
-*/
+/////////////// Bitmap
 
-// Please UNCOMMENT one of the contructor lines below
-// U8g2 Contructor List (Frame Buffer)
-// The complete list is available here: https://github.com/olikraus/u8g2/wiki/u8g2setupcpp
-    U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-//      U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-// End of constructor list
+
+const unsigned char upir_logo [] PROGMEM = {  
+  0xEA, 0x3A, 0xAA, 0x28, 0x6A, 0x1A, 0x26, 0x2A, };
+
+
+// 'icon_3dcube', 16x16px
+const unsigned char bitmap_icon_3dcube [] PROGMEM = {
+  0x00, 0x00, 0x80, 0x01, 0xE0, 0x06, 0x98, 0x18, 0x86, 0x60, 0x8A, 0x50, 
+  0xA2, 0x45, 0x82, 0x40, 0xA2, 0x44, 0x82, 0x40, 0xA2, 0x45, 0x8A, 0x50, 
+  0x86, 0x60, 0x98, 0x18, 0xE0, 0x06, 0x80, 0x01, };  
+// 'icon_battery', 16x16px
+const unsigned char bitmap_icon_battery [] PROGMEM = {
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFC, 0x1F, 0x02, 0x20, 
+  0xDA, 0x66, 0xDA, 0x66, 0xDA, 0x66, 0x02, 0x20, 0xFC, 0x1F, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, };
+// 'icon_dashboard', 16x16px
+const unsigned char bitmap_icon_dashboard [] PROGMEM = {
+  0xE0, 0x07, 0x18, 0x18, 0x84, 0x24, 0x0A, 0x40, 0x12, 0x50, 0x21, 0x80, 
+  0xC1, 0x81, 0x45, 0xA2, 0x41, 0x82, 0x81, 0x81, 0x05, 0xA0, 0x02, 0x40, 
+  0xD2, 0x4B, 0xC4, 0x23, 0x18, 0x18, 0xE0, 0x07, };
+// 'icon_fireworks', 16x16px
+
+
+// Array of all bitmaps for convenience. (Total bytes used to store images in PROGMEM = 384)
+const unsigned char* bitmap_icons[3] = {
+  bitmap_icon_3dcube,
+  bitmap_icon_battery,
+  bitmap_icon_dashboard
+};
+
+
+// 'scrollbar_background', 8x64px
+const unsigned char bitmap_scrollbar_background [] PROGMEM = {
+  0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 
+  0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 
+  0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 
+  0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 
+  0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 0x00, 0x40, 
+  0x00, 0x40, 0x00, 0x00, };
+
+
+// 'item_sel_outline', 128x21px
+const unsigned char bitmap_item_sel_outline [] PROGMEM = {
+  0xF8, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+  0xFF, 0xFF, 0xFF, 0x03, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 0x02, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0C, 
+  0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+  0x00, 0x00, 0x00, 0x0C, 0xFC, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x07, 0xF8, 0xFF, 0xFF, 0xFF, 
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x03, 
+  };
+
+
+
+//////////////// End
+
+#define BUTTON_SELECT_PIN 7 // pin for SELECT button
+#define BUTTON_UP_PIN 4 // pin for UP button 
+#define BUTTON_DOWN_PIN 5 // pin for DOWN button
+#define BUTTON_CHANGE_PIN 8
+
+
+ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+
+const int NUM_ITEMS = 3; // number of items in the list and also the number of screenshots and screenshots with QR codes (other screens)
+const int MAX_ITEM_LENGTH = 20; // maximum characters for the item name
+
+char menu_items [NUM_ITEMS] [MAX_ITEM_LENGTH] = {  // array with item names
+  { "Item1" }, 
+  { "Mode" }, 
+  { "Item3" }, 
+ };
+
+int button_up_clicked = 0; // only perform action when button is clicked, and wait until another press
+int button_select_clicked = 0; // same as above
+int button_down_clicked = 0; 
+
+int item_selected = 0; // which item in the menu is selected
+int item_sel_previous; // previous item - used in the menu screen to draw the item before the selected one
+int item_sel_next; // next item - used in the menu screen to draw next item after the selected one
+  
+
+
+
 
 int n=0;
 int n1=0;
@@ -90,28 +142,33 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;  
 
 
+
+
+
 void setup(void) {
-  
-  //Serial.begin(115200);
+
   u8g2.begin();
-  pinMode(8, INPUT_PULLUP);
+  pinMode(BUTTON_CHANGE_PIN, INPUT_PULLUP);
 
-  // Init timer ITimer1
-  //ITimer1.init();
+  pinMode(BUTTON_UP_PIN, INPUT_PULLUP); // up button
 
-  //u8g2.clearBuffer();					// clear the internal memory
-  //u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
-  //u8g2.drawStr(0,10,"Hello World!");	// write something to the internal memory
-  //u8g2.sendBuffer();					// transfer internal memory to the display
+  pinMode(BUTTON_SELECT_PIN, INPUT_PULLUP); // select button
+
+  pinMode(BUTTON_DOWN_PIN, INPUT_PULLUP); // down button
+
   delay(1000);
-  //randomSeed(analogRead(0));
+
 }
 
-
+int ti = 60;
+int v1 = 100;
+int v2 = 200;
 
 void loop(void) {
 
-  int reading = digitalRead(8);
+    
+
+  int reading = digitalRead(BUTTON_CHANGE_PIN);
 
    // If the switch changed, due to noise or pressing:
   if (reading ==LOW && lastButtonState==HIGH) {
@@ -128,7 +185,7 @@ void loop(void) {
     if (reading == LOW) {
       //buttonState = reading;
       ScreenIndex += 1; 
-    if (ScreenIndex >= 2) {
+    if (ScreenIndex >= 3) {
         ScreenIndex = 0;
     }
 
@@ -160,23 +217,15 @@ void loop(void) {
 
   //u8g2.firstPage();
   //do {
-
-    VoltMeasure=5+5*sin(n1*0.1);
-  //Serial.print("n = ");Serial.print(n);Serial.print(" ;   ->  Bat Volt = ");Serial.print(BatteryV[n]);
-  BatteryV[n]=PIXELRANGE+3-VoltMeasure*PIXELRANGE/VOLTRANGE;    // convert voltage value into pixel height
-
-  if(n==ARRAYSIZE-1){ for(j=0;j<ARRAYSIZE;j++) BatteryV[j]=BatteryV[j+1];} else n++;
-  //if(n==ARRAYSIZE-1) n=0; else n++;
-  n1++; 
-  
-
     if (ScreenIndex == 0){
 
     
   u8g2.clearBuffer();					// clear the internal memory
   
   //BatteryV[n]=5+5*sin(n1*0.1);
-  
+  VoltMeasure=5+5*sin(n1*0.1);
+  //Serial.print("n = ");Serial.print(n);Serial.print(" ;   ->  Bat Volt = ");Serial.print(BatteryV[n]);
+  BatteryV[n]=PIXELRANGE+3-VoltMeasure*PIXELRANGE/VOLTRANGE;    // convert voltage value into pixel height
   //Serial.print("  pixel height = ");Serial.println(BatteryV[n]);
 
   //testecheck=(5+5*sin(n1*0.1))*10;
@@ -205,7 +254,9 @@ void loop(void) {
   //delay(100);
   
   //delay(1000);
-   
+  if(n==ARRAYSIZE-1){ for(j=0;j<ARRAYSIZE;j++) BatteryV[j]=BatteryV[j+1];} else n++;
+  //if(n==ARRAYSIZE-1) n=0; else n++;
+  n1++;  
   
   //} while(u8g2.nextPage());
   //u8g2.clearDisplay();
@@ -223,8 +274,217 @@ void loop(void) {
     changeDisplayContent();
   }
 
+  if (ScreenIndex == 2) {
+    menu(); // LOW = pressed
+
+     if ((digitalRead(BUTTON_UP_PIN) == LOW) && (button_up_clicked == 0)) { // up button clicked - jump to previous menu item
+        item_selected = item_selected - 1; // select previous item
+        button_up_clicked = 1; // set button to clicked to only perform the action once
+        if (item_selected < 0) { // if first item was selected, jump to last item
+          item_selected = NUM_ITEMS-1;
+        }
+      }
+
+     else if ((digitalRead(BUTTON_DOWN_PIN) == LOW) && (button_down_clicked == 0)) { // down button clicked - jump to next menu item
+        item_selected = item_selected + 1; // select next item
+        button_down_clicked = 1; // set button to clicked to only perform the action once
+        if (item_selected >= NUM_ITEMS) { // last item was selected, jump to first menu item
+          item_selected = 0;
+          }
+      } 
+
+      if ((digitalRead(BUTTON_UP_PIN) == HIGH) && (button_up_clicked == 1)) { // unclick 
+        button_up_clicked = 0;
+      }
+
+      if ((digitalRead(BUTTON_DOWN_PIN) == HIGH) && (button_down_clicked == 1)) { // unclick
+        button_down_clicked = 0;
+      }
+
+  }
+
+if (item_selected == 0) {
+  if ((digitalRead(BUTTON_SELECT_PIN) == LOW) && (button_select_clicked == 0)) { // select button clicked, jump between screens
+      button_select_clicked = 1; // set button to clicked to only perform the action once
+     if (ScreenIndex == 2) {ScreenIndex = 3;} // menu items screen --> variable screen
+     else if (ScreenIndex == 3) {ScreenIndex = 2;} // variable screen --> menu items screen
+  }
+
+  if ((digitalRead(BUTTON_SELECT_PIN) == HIGH) && (button_select_clicked == 1)) { // unclick 
+    button_select_clicked = 0;
+  }
 }
 
+if (item_selected == 1) {
+  if ((digitalRead(BUTTON_SELECT_PIN) == LOW) && (button_select_clicked == 0)) { // select button clicked, jump between screens
+      button_select_clicked = 1; // set button to clicked to only perform the action once
+     if (ScreenIndex == 2) {ScreenIndex = 4;} // menu items screen --> variable screen
+     else if (ScreenIndex == 4) {ScreenIndex = 2;} // variable screen --> menu items screen
+  }
+
+  if ((digitalRead(BUTTON_SELECT_PIN) == HIGH) && (button_select_clicked == 1)) { // unclick 
+    button_select_clicked = 0;
+  }
+}
+
+if (item_selected == 2) {
+  if ((digitalRead(BUTTON_SELECT_PIN) == LOW) && (button_select_clicked == 0)) { // select button clicked, jump between screens
+      button_select_clicked = 1; // set button to clicked to only perform the action once
+     if (ScreenIndex == 2) {ScreenIndex = 5;} // menu items screen --> variable screen
+     else if (ScreenIndex == 5) {ScreenIndex = 2;} // variable screen --> menu items screen
+  }
+
+  if ((digitalRead(BUTTON_SELECT_PIN) == HIGH) && (button_select_clicked == 1)) { // unclick 
+    button_select_clicked = 0;
+  }
+}
+
+
+  if (ScreenIndex == 3) 
+  { 
+    u8g2.clearBuffer();
+    u8g2.setCursor(20, 45);
+    u8g2.print("Variable: ");
+    
+    
+
+    if ((digitalRead(BUTTON_UP_PIN) == LOW) && (button_up_clicked == 0)) { // up button clicked - jump to previous menu item
+        ti += 3;
+        button_up_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_UP_PIN) == HIGH) && (button_up_clicked == 1)) { // unclick 
+        button_up_clicked = 0;
+      } 
+
+      //u8g2.print(ti);
+
+
+    //u8g2.sendBuffer();
+
+    if ((digitalRead(BUTTON_DOWN_PIN) == LOW) && (button_down_clicked == 0)) { // down button clicked - jump to previous menu item
+        ti -= 3;
+        button_down_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_DOWN_PIN) == HIGH) && (button_down_clicked == 1)) { // unclick 
+        button_down_clicked = 0;
+      } 
+
+      u8g2.print(ti);
+
+
+    u8g2.sendBuffer();
+
+    
+  }
+
+    
+  //}
+
+  
+
+  if (ScreenIndex == 4) 
+  { 
+    u8g2.clearBuffer();
+    u8g2.setCursor(20, 45);
+    u8g2.print("Variable1: ");
+
+    if (v1 == 100) { 
+        u8g2.print("low");
+        u8g2.sendBuffer();
+      }
+
+    if (v1 == 103) { 
+        u8g2.print("eco");
+        u8g2.sendBuffer();
+      }
+
+    if (v1 == 106) { 
+        u8g2.print("power");
+        u8g2.sendBuffer();
+      }
+
+    if ((digitalRead(BUTTON_UP_PIN) == LOW) && (button_up_clicked == 0)) { // up button clicked - jump to previous menu item
+        if (v1 <= 103) {
+          v1 += 3;
+        }
+
+        button_up_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_UP_PIN) == HIGH) && (button_up_clicked == 1)) { // unclick 
+        button_up_clicked = 0;
+      } 
+
+      //u8g2.print(ti);
+
+
+    //u8g2.sendBuffer();
+
+    if ((digitalRead(BUTTON_DOWN_PIN) == LOW) && (button_down_clicked == 0)) { // down button clicked - jump to previous menu item
+        if (v1 >= 103) {
+          v1 -= 3;
+        }
+        //v1 -= 3;
+        button_down_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_DOWN_PIN) == HIGH) && (button_down_clicked == 1)) { // unclick 
+        button_down_clicked = 0;
+      } 
+
+      //u8g2.print(v1);
+
+
+    u8g2.sendBuffer();
+  }
+
+
+
+  if (ScreenIndex == 5) 
+  { 
+    u8g2.clearBuffer();
+    u8g2.setCursor(20, 45);
+    u8g2.print("Variable2: ");
+
+
+     if ((digitalRead(BUTTON_UP_PIN) == LOW) && (button_up_clicked == 0)) { // up button clicked - jump to previous menu item
+        v2 += 3;
+        button_up_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_UP_PIN) == HIGH) && (button_up_clicked == 1)) { // unclick 
+        button_up_clicked = 0;
+      } 
+
+      //u8g2.print(ti);
+
+
+    //u8g2.sendBuffer();
+
+    if ((digitalRead(BUTTON_DOWN_PIN) == LOW) && (button_down_clicked == 0)) { // down button clicked - jump to previous menu item
+        v2 -= 3;
+        button_down_clicked = 1;
+      }
+
+      if ((digitalRead(BUTTON_DOWN_PIN) == HIGH) && (button_down_clicked == 1)) { // unclick 
+        button_down_clicked = 0;
+      } 
+
+      u8g2.print(v2);
+
+
+    u8g2.sendBuffer();
+  }
+
+  // set correct values for the previous and next items
+  item_sel_previous = item_selected - 1;
+  if (item_sel_previous < 0) {item_sel_previous = NUM_ITEMS - 1;} // previous item would be below first = make it the last
+  item_sel_next = item_selected + 1;  
+  if (item_sel_next >= NUM_ITEMS) {item_sel_next = 0;} // next item would be after last = make it the first
+
+}
 
 
 void changeDisplayContent() {
@@ -257,13 +517,13 @@ void changeDisplayContent() {
   u8g2.print("%");
 
   // Display Power
-  u8g2.setCursor(0, 25);
+  u8g2.setCursor(0, 20);
   u8g2.print("M: Power");
 
   // Display Ti
-  u8g2.setCursor(60, 25);
+  u8g2.setCursor(60, 20);
   u8g2.print("Ti: ");
-  u8g2.setCursor(80, 25);
+  u8g2.setCursor(80, 20);
   u8g2.print(ti);
   u8g2.print("min");
 
@@ -298,7 +558,48 @@ void changeDisplayContent() {
   u8g2.sendBuffer();
 
 
-
   
 }
 
+
+
+
+void menu() {
+  u8g2.setFont(u8g_font_7x14);
+  u8g2.clearBuffer();
+
+  //u8g2.setCursor(50, 12);
+  //u8g2.print("Menu");
+
+
+      // selected item background
+      u8g2.drawXBMP(0, 22, 128, 21, bitmap_item_sel_outline);
+
+      // draw previous item as icon + label
+      u8g2.setFont(u8g_font_7x14);
+      u8g2.drawStr(25, 15, menu_items[item_sel_previous]); 
+      u8g2.drawXBMP( 4, 2, 16, 16, bitmap_icons[item_sel_previous]);          
+
+      // draw selected item as icon + label in bold font
+      u8g2.setFont(u8g_font_7x14B);    
+      u8g2.drawStr(25, 15+20+2, menu_items[item_selected]);   
+      u8g2.drawXBMP( 4, 24, 16, 16, bitmap_icons[item_selected]);     
+
+      // draw next item as icon + label
+      u8g2.setFont(u8g_font_7x14);     
+      u8g2.drawStr(25, 15+20+20+2+2, menu_items[item_sel_next]);   
+      u8g2.drawXBMP( 4, 46, 16, 16, bitmap_icons[item_sel_next]);  
+
+      // draw scrollbar background
+      u8g2.drawXBMP(128-8, 0, 8, 64, bitmap_scrollbar_background);
+
+      // draw scrollbar handle
+      u8g2.drawBox(125, 64/NUM_ITEMS * item_selected, 3, 64/NUM_ITEMS); 
+
+   
+        
+       
+  u8g2.sendBuffer(); // send buffer from RAM to display controller
+
+/////////////
+}
